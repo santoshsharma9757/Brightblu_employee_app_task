1d6496950a19d757caa00e2d310da53bb48240a0
santoshkumarlohar <santoshkumar.l@detecttechnologies.com>
2024-08-29 14:26:26 +0530

    brightblue employee app completed

--- brightblu_user_info/android/app/src/main/kotlin/com/example/brightblu_user_info/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.brighblue_test.brightblu_employee_info
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- brightblu_user_info/lib/constant/app_sftp_cred.dart
+++
@@ -1,6 +0,0 @@
-class AppSftpCread {
-  static const String host="ap-southeast-1.sftpcloud.io";
-  static const int port=22;
-  static const String username="flutterDev";
-  static const String password="qLvS8YEjqZBpCRjnVvzBT9SvYdYtNFEE";
-}
